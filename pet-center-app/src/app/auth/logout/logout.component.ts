2918a1546f86cfaa53218d73bc48b895bad24633
MiroZe <miro_sv@yahoo.com>
2023-08-09 00:28:25 +0300

    some minor css changes

--- pet-center-app/src/app/auth/logout/logout.component.ts
+++ pet-center-app/src/app/auth/logout/logout.component.ts
@@ -3,7 +3,10 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { MessageDispatcherService } from 'src/app/core/message-dispatcher.service';
 import { MessageType } from 'src/app/interfaces/messages';
-import { MatDialog } from '@angular/material/dialog';
+
+import { DialogConfirmationService } from 'src/app/shared/dialog-confirmation.service';
+import { EMPTY, switchMap } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-logout',
@@ -11,19 +14,39 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent {
+
+  previousUrl! : string | null
+   
   constructor(
     private authService: AuthService,
     private router: Router,
     private messageDispatcher: MessageDispatcherService,
-    
+    private confirmationService: DialogConfirmationService,
   ) {
 
-    this.authService.logout$().subscribe(() => {
+    
+    this.confirmationService.openDialog('logout').pipe(
+      switchMap(result => {
+        
+        if(result) {
+          return this.authService.logout$()
+          
+        }else {
+         
+          
+          
+          this.router.navigate(['/'])
+          return EMPTY
+        }
+      })
+    ).subscribe(() => {
       this.messageDispatcher.notifyForMessage({
         text: 'You has been successfuly logged out',
         type: MessageType.Success,
       });
       this.router.navigate(['/']);
-    });
+    })
+
+   
   }
 }
